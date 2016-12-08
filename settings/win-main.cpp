4a87a6d94679e82720f448bbd506af9a56a3322e
jeizas <jeizas1210@gmail.com>
2016-12-08 11:55:35 +0800

    midify win-main.cpp

--- settings/win-main.cpp
+++ settings/win-main.cpp
@@ -18,16 +18,20 @@ int main() {
     char confDir[] = "../SD_WLAN/CONFIG";
     char imgDefultDir[] = "FA000001.JPG";
     char imgSdDir[] = "../DCIM/100__TSB/FA000001.JPG";
-    string copyState = (copyFile(imgDefultDir, imgSdDir) == 1) ? "success" : "failure";
+    //copy default file.
+		string copyState = (copyFile(imgDefultDir, imgSdDir) == 1) ? "success" : "failure";
     cout << "copy default image .... " << copyState << endl;
     
-	srand(time(0));
-	int num = 0;
-	num = (rand() % 90000) + 100000;
-	string str;
-	stringstream strStream;
-	strStream << num;
-	str = strStream.str();
+		//Reset random number
+		srand(time(0));
+		int num = 0;
+		num = (rand() % 90000) + 100000;
+		//int to str with stringstream
+		string str;
+		stringstream strStream;
+		strStream << num;
+		str = strStream.str();
+		//setting config file
     string wifiMsg = "APPSSID=alltuu" + str + "\nAPPNETWORKKEY=12345678";
     string addState = addContentToFile(confDir, wifiMsg) == 1 ? "success" : "failure";
     cout << "add wifi massage :\n"<< wifiMsg << " .... " << addState << endl;
@@ -49,8 +53,9 @@ int copyFile(char *filename, char *newfile) {
         return 0;
     }
     //chmod(newfile, S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH);
-	SetFileAttributes(newfile, FILE_ATTRIBUTE_NORMAL);
-	out.open(newfile, ios::binary);
+		//modify permissiom for target file. FILE_ATTRIBUTE_NORMAL.
+		SetFileAttributes(newfile, FILE_ATTRIBUTE_NORMAL);
+	  out.open(newfile, ios::binary);
     out << in.rdbuf() << endl;
     out.close();
     in.close();
